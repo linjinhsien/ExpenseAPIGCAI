01cded03b25943db734843c10b28697852da6c25
linjinhsienmsi <iven840320@gmail.com>
2024-10-04 21:14:18 +0800

    增加dbcontext

---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,15 @@
+//根據Expense.cs創建dbcontext
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseAPI.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
